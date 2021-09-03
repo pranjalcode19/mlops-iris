bcd4d7700820ab42f55db423147e5acff71093a2
Pranjal Kesharwani <pranjalkesharwani7@gmail.com>
2021-09-03 19:54:05 +0530

    Completing build

--- test_app.py
+++ test_app.py
@@ -1,26 +1,45 @@
 from fastapi.testclient import TestClient
 from main import app
 
-# test to check the correct functioning of the /ping route
+
 def test_ping():
     with TestClient(app) as client:
         response = client.get("/ping")
-        # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"ping": "pong"}
+        assert response.json() == {"ping":"pong"}
 
-
-# test to check if Iris Virginica is classified correctly
 def test_pred_virginica():
-    # defining a sample payload for the testcase
     payload = {
-        "sepal_length": 3,
-        "sepal_width": 5,
-        "petal_length": 3.2,
-        "petal_width": 4.4,
+      "sepal_length": 3,
+      "sepal_width": 5,
+      "petal_length": 3.2,
+      "petal_width": 4.4
     }
     with TestClient(app) as client:
-        response = client.post("/predict_flower", json=payload)
-        # asserting the correct response is received
+        response = client.post('/predict_flower', json=payload)
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json() == {'flower_class': "Iris Virginica"}
+
+def test_pred_Versicolour():
+    payload = {
+            "sepal_length": 5.7,
+            "sepal_width": 2.9,
+            "petal_length": 4.2,
+            "petal_width": 1.3
+        }
+    with TestClient(app) as client:
+        response = client.post('/predict_flower', json=payload)
+        assert response.status_code == 200
+        assert response.json() == {'flower_class': "Iris Versicolour"}
+
+def test_pred_Setosa():
+    payload = {
+            "sepal_length": 4.6,
+            "sepal_width": 3.1,
+            "petal_length": 1.5,
+            "petal_width": 0.2
+        }
+    with TestClient(app) as client:
+        response = client.post('/predict_flower', json=payload)
+        assert response.status_code == 200
+        assert response.json() == {'flower_class': "Iris Setosa"}
